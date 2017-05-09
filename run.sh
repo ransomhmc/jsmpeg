7002c6939970bddf7f27e962dcaccea7d13ab53e
ransom <ransomhmc@gmail.com>
2017-05-09 20:19:23 +0800

    scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+PORT=80
+if [ ! -z "$1" ]
+  then
+    PORT=$1
+fi
+echo "port=$PORT"
 sudo setcap 'cap_net_bind_service=+ep' ~/.nvm/versions/node/v7.5.0/bin/node
 node websocket-relay.js aloha 8888 9999 &
-node_modules/.bin/http-server -c-1 -p 80
+nohup node_modules/.bin/http-server -c-1 -p $PORT &
 
