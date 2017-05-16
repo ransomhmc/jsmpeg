6e8334ede22597fa3af5fb08fcbf76cffb2b9e3b
ransom <ransomhmc@gmail.com>
2017-05-16 23:25:56 +0800

    encode stream as 1M, 20fps

--- getstream.sh
+++ getstream.sh
@@ -2,7 +2,7 @@
 while true
 do
   node websocket-relay.js aloha 8888 9999 &
-  ffmpeg -i rtmp://54.169.82.73:1935/live/testbed -vcodec mpeg1video -b:v 2M -r 24 -f mpegts http://127.0.0.1:8888/aloha
+  ffmpeg -i rtmp://54.169.82.73:1935/live/testbed -vcodec mpeg1video -b:v 1024k -r 20 -f mpegts http://127.0.0.1:8888/aloha
   sleep 5
 done
 
