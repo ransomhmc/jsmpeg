ffcd23715d0654c12cef1c52e22e8e2acb1d53eb
ransom <ransomhmc@gmail.com>
2017-05-23 22:24:29 +0800

    relay function combined

--- getstream.sh
+++ getstream.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 while true
 do
-  node websocket-relay.js aloha 8888 9999 &
   ffmpeg -i rtmp://54.169.82.73:1935/live/testbed -vcodec mpeg1video -b:v 1024k -r 20 -f mpegts http://127.0.0.1:8888/aloha
   sleep 5
 done
